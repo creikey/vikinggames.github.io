8b65ce773ffa5fc0257c413f982464bdab6acc7d
Cameron Reikes <cameronreikes@gmail.com>
2018-02-01 23:54:39 -0800

    Optimize website for mobile

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(function() {
   var home = "Home"
   var calendar = "Calendar"
   var agenda = "Agenda"
+  var resources = "Resources"
   var calendarURL = "https://calendar.google.com/calendar/embed?src=i33vrlk3e1bj7th9ruqk04sgak%40group.calendar.google.com&ctz=America%2FLos_Angeles"
   var path = window.location.pathname;
   var page = path.split("/").pop();
@@ -10,6 +11,9 @@ $(function() {
   }
   if (page == "agenda.html") {
     agenda= "<b>Agenda</b>"
+  }
+  if (page == "resources.html") {
+    resources = "<b>Resources</b>"
   }
   var html = [
     '<div class="title">',
@@ -20,6 +24,7 @@ $(function() {
         '<li><a href="index.html">' + home + '</a></li>',
         '<li><a target="_blank" href="' + calendarURL + '">' + calendar + '</a></li>',
         '<li><a href="agenda.html">' + agenda + '</a></li>',
+        '<li><a href="resources.html">' + resources + '</a></li>',
       '</ul>',
     '</div>'
   ].join("\n");
