6a367066040688ec89301b69d90f6a681bcf2bb2
Cameron Reikes <cameronreikes@gmail.com>
2018-02-08 01:49:26 -0800

    Add discord and github links

--- js/script.js
+++ js/script.js
@@ -18,6 +18,13 @@ $(function() {
     calendar = "<b>Calendar</b>"
   }
   var html = [
+    '<div class="links">',
+      '<span id="center">',
+        '<a href="https://discordapp.com/invite/5cZQAv3" target="_blank"><img id="center" class="bg" src="css/discord_logo.png" alt="Discord"></a>',
+      '</span>',
+      '<a id="right" href="https://www.sandiegounified.org/schools/la-jolla" target="_blank"><img class="bg" src="css/ljhs_logo.png" alt="La Jolla High School"></a>',
+      '<a id="left" href="https://github.com/vikinggames" target="_blank"><img src="css/github_logo.png" alt="Github"></a>',
+    '</div>',
     '<div class="title">',
       '<h1>Viking Games</h1>',
     '</div>',
@@ -27,7 +34,6 @@ $(function() {
         '<li><a href="calendar.html">' + calendar + '</a></li>',
         '<li><a href="agenda.html">' + agenda + '</a></li>',
         '<li><a href="resources.html">' + resources + '</a></li>',
-      '<li><img src="css/ljhs_logo.png"></li>',
       '</ul>',
     '</div>'
   ].join("\n");
